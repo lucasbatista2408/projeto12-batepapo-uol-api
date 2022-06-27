0aa5c9a24edda7b9fd3d770fc689ea03e4613729
lucasbatista2408 <lucasbatista2408@gmail.com>
2022-06-26 23:56:54 -0300

    all set

--- index.js
+++ index.js
@@ -22,66 +22,133 @@ client.connect().then(() => {
   db = client.db('batepapoapi')
 })
 
+const schemaParticipants = Joi.object({
+  name: Joi.string().min(1).required(),
+});
+
+const schemaMessage = Joi.object({
+  to: Joi.string().min(1).required(),
+  text: Joi.string().min(1).required(),
+  type: Joi.string().valid("message", "private_message").required()
+})
 
 //PARTICIPANTS 
 app.post('/participants', async (req, res) =>{
-  const {name} = req.body;
+  const name = req.body;
+  const validation = schemaParticipants.validate(name, { abortEarly: true });
 
-  const participant = {
-    name,
-    lastStatus: Date.now()
-  }
-  const welcome = {
-    from: name, 
-    to: 'Todos', 
-    text: 'entra na sala...', 
-    type: 'status', 
-    time: dayjs().format('HH:mm:ss')
+  if(validation.error){
+    return res.sendStatus(422);
   }
 
-  participants = await db.collection('participants').find().toArray()
-  const exists = participants.find(participant => participant.name === name)
+  try{
 
-  if(exists){
-    res.status(409).send('conflito')
-    return
-  } else{
-    const userADD = await db.collection('participants').insertOne(participant)
+    const exists = await db.collection("participants").find({...name}).toArray();
+
+    if(exists.length > 0){
+      console.log("Duplicated");
+      return res.status(409).send("Already Exists");
+    }
+
+    const participant = {
+      name: name.name,
+      lastStatus: Date.now()
+    }
+    const welcome = {
+      from: name.name, 
+      to: 'Todos', 
+      text: 'entra na sala...', 
+      type: 'sendStatus', 
+      time: dayjs().format('HH:mm:ss')
+    }
+
+    await db.collection('participants').insertOne(participant)
     await db.collection('messages').insertOne(welcome)
-    res.send(userADD)
+    res.status(201).send("SUCESSFULL");
+
+  } catch(err){
+    console.log(err);
+    res.status(422).send("Erro ao cadastrar participante");
   }
-  res.status(201)
 })
 
 app.get('/participants', async (req,res) => {
   participants = await db.collection('participants').find().toArray()
-  res.send(participants)
+  res.status(201).send(participants)
 })
 
 // MESSAGES
 app.post('/messages', async (req, res) => {
-  const {to, text, type} = req.body;
-  const { User } = req.headers;
+  const data = req.body;
+  const validate = schemaMessage.validate(data, { abortEarly: true });
+  const { user } = req.headers;
+
   const message = {
-    from: User,
-    to,
-    text,
-    type,
+    from: user,
+    to: data.to,
+    text: data.text,
+    type: data.type,
     time: dayjs().format('HH:mm:ss')
   }
- await db.collection('messages').insertOne(message)
- res.status(201)
 
+  if(validate.error){
+    console.log(validate.error)
+    return res.sendStatus(422);
+  }
+
+  try{
+    await db.collection('messages').insertOne(message)
+    res.sendStatus(201)
+  } catch(err){
+    console.log(err);
+    res.sendStatus(422);
+  }
 })
 
 app.get('/messages', async (req, res) => {
-  const messages = await db.collection('messages').find().toArray()
-  res.send(messages)
+
+  const limit = req.query.limit;
+
+  try{
+    const visible =  await db.collection("messages").find({$or: [{type: "message"}, {to: "Todos"}, {to: req.headers.user}, {from: req.headers.user}, ]}).sort({time: 1}).toArray();
+    res.status(200).send(visible.slice(-limit));
+  }catch(err){
+    console.log(err);
+    res.sendStatus(500);
+  }
 })
 
-// STATUS
-app.post('/status', (req, res) =>{
+// Status
+app.post('/status', async (req, res) =>{
+  const {user} = req.headers;
 
+  try{
+  const status = await db.collection('participants').findOne({name: user})
+    if(!status){
+      console.log("NOT_FOUND")
+      res.sendStatus(404).send("NOT FOUND")
+    }
+
+    const online = await db.collection('participants').updateOne({name: user},
+      {$set:
+      {lastStatus: Date.now()}})
+    
+    const tobeDeleted = await db.collection('participants').findONe({lastStatus: {$lt: Date.now() - 15000}})
+    const offline = tobeDeleted.name;
+    const offlineMessage = {
+      from: offline, 
+      to: "Todos", 
+      text: `saiu da sala...`, 
+      type: "status", 
+      time: dayjs().format('HH:mm:ss')
+    }
+    await db.collection('messages').insertOne(offlineMessage)
+    await db.collection('participants').deleteOne({name: offline})
+    res.status(200).send("UPDATED")
+  } catch (err){
+    console.log(err);
+    res.status(500).send("COULD NOT UPDATE")
+  }
 })
 
 app.listen(process.env.PORT, () => {
